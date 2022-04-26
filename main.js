15935d496483ba87c387881b953f333c09d83e6f
fullMoon Admin <coordinator@fullmoonprod.com>
2022-04-26 14:06:32 -0500

    hard coded in path

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
+
 //import dependencies 
 const { SerialPort } = require('serialport')
 const { ReadlineParser } = require('@serialport/parser-readline')
 
-// the path variable is a command line argument 
-const port = new SerialPort({ path: argv[2], baudRate: 9600 })
+// use applicable path variable
+const port = new SerialPort({ path: '/dev/ttyACM0', baudRate: 9600 })
 
 //delimiter cleans up output with a return and a new line
 const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }))
 
-//console logs data
+//console logs
 parser.on('data', (line) => {
   console.log(line)
 }
